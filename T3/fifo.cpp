feb37645ec74c61dc07e15e1dcac63dfa5d5f097
Mateusz Ostaszewski <mateuszostaszewskififa@gmail.com>
2023-12-07 12:51:32 +0100

    fifo updated

--- T3/fifo.cpp
+++ T3/fifo.cpp
@@ -1,13 +1,14 @@
 #include <semaphore>
 #include <vector>
+# include <counti
 
 class FifoBuffer {
 private:
     std::vector<int> buffer;
     size_t capacity;
-    std::counting_semaphore<1> mutex; // Semafor binarny do ochrony sekcji krytycznej
-    std::counting_semaphore<> empty;  // Semafor do sygnalizowania, że bufor jest pusty
-    std::counting_semaphore<> full;   // Semafor do sygnalizowania, że bufor jest pełny
+    Semaphore mutex; // Semafor binarny do ochrony sekcji krytycznej
+    Semaphore empty;  // Semafor do sygnalizowania, że bufor jest pusty
+    Semaphore full;   // Semafor do sygnalizowania, że bufor jest pełny
 
     int evenCount;
     int oddCount;
@@ -16,25 +17,25 @@ public:
     FifoBuffer(size_t cap) : capacity(cap), mutex(1), empty(cap), full(0), evenCount(0), oddCount(0) {}
 
     void put(int value) {
-        full.acquire(); // Czeka, aż będzie miejsce w buforze
-        mutex.acquire(); // Blokuje dostęp do bufora
+        full.p(); // Czeka, aż będzie miejsce w buforze
+        mutex.p(); // Blokuje dostęp do bufora
         buffer.push_back(value);
         updateCountsOnPut(value);
-        mutex.release(); // Zwalnia dostęp do bufora
-        empty.release(); // Sygnalizuje, że bufor nie jest pusty
+        mutex.v(); // Zwalnia dostęp do bufora
+        empty.v(); // Sygnalizuje, że bufor nie jest pusty
     }
 
     int get(bool consumeEven) {
         int value = 0;
-        empty.acquire(); // Czeka, aż bufor będzie miał elementy
-        mutex.acquire(); // Blokuje dostęp do bufora
+        empty.p(); // Czeka, aż bufor będzie miał elementy
+        mutex.p(); // Blokuje dostęp do bufora
         if (!buffer.empty() && (consumeEven == isEven(buffer.front()))) {
             value = buffer.front();
             buffer.erase(buffer.begin());
             updateCountsOnGet(value);
         }
-        mutex.release(); // Zwalnia dostęp do bufora
-        full.release(); // Sygnalizuje, że w buforze jest miejsce
+        mutex.v(); // Zwalnia dostęp do bufora
+        full.v(); // Sygnalizuje, że w buforze jest miejsce
         return value;
     }
 
